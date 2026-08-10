951dfb81cb5e72f48d437dac12996aca7800b389
Sarthak Kawade <kawadesarthak386@gmail.com>
2026-08-10 15:21:39 +0530

    Added vercel.json for React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { EventProvider } from "./context/EventContext";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
- <EventProvider>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-</EventProvider>
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
 );
